4ca4185762c1dea276dd64c5d388367a74a27bc8
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-01-30 18:22:35 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, first_name, last_name, email, password, status="inactive"):
+        self.f_name = first_name
+        self.l_name = last_name
+        self.email = email
+        self.password = password
+        self.status = status
+
+    def display_users(self):
+        pass
+
+    def new_user(self):
+        pass
+
+    def main_list(self):
+        pass
+
+    def add_user(self):
+        pass
+
+    def show_all_users(self):
+        pass
